7cd532428fc0876884e0f8a02673819a3b57867a
ÄĆmaticmilica <maticmilica1912@gmail.com>
2016-03-14 21:55:55 +0100

    new version1

--- main.c
+++ main.c
@@ -6,5 +6,12 @@ int main()
     printf("Hello world!\n");
 	printf("edited code");
 	printf("Team2 was here");
-    return 0;
+	
+	int a, b, c;
+	a = 2;
+	b = 3;
+	c = a+b;
+	printf("%d", c);
+    
+	return 0;
 }
